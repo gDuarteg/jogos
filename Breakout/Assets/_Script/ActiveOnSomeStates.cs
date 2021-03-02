fd5109ba5a6596c56390cdd4247d304095a12ca4
Gabriel Duarte <gabrielmdk01@gmail.com>
2021-03-02 02:00:45 -0300

    Breakout 400xp

---
+++ Breakout/Assets/_Script/ActiveOnSomeStates.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ActiveOnSomeStates : MonoBehaviour
+{
+
+    public GameManager.GameState[] activeStates;
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+        GameManager.changeStateDelegate += UpdateVisibility;
+        UpdateVisibility();
+    }
+
+    void UpdateVisibility()
+    {
+        if(activeStates.Contains(gm.gameState))
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Breakout/Assets/_Script/UI_MenuPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MenuPrincipal : MonoBehaviour
+{
+    GameManager gm;
+
+    private void OnEnable()
+    {
+        gm = GameManager.GetInstance();      
+    }
+
+    public void Comecar()
+    {
+        gm.changeState(GameManager.GameState.GAME);
+    }
+}
